14a6cc5302d53d3de31bcd8394f034f9b3b4e3dc
Teresa <teresa.edinger@gmail.com>
2020-10-12 15:53:48 +0200

    bsp 1 started

--- src/hausuebung4/bsp1/Main.java
+++ src/hausuebung4/bsp1/Main.java
@@ -5,15 +5,18 @@
  */
 package hausuebung4.bsp1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 /**
  *
@@ -30,20 +33,66 @@ public class Main {
         System.out.println("In wie viele Unterteile soll die Liste aufgeteilt werden?");
         int chunks = Integer.parseInt(scanner.nextLine());
 
+        Main main = new Main();
+        main.readFile();
+        //for (int i = 0; i < main.list.size(); i++) {
+        //  System.out.print(main.list.get(i) + " ");
+        //}
+
+        System.out.println(main.list.size());
+        System.out.println(main.toIntList().size());
+
+        List<Integer> list = main.toIntList();
+        List<Integer> teilbareNr = new ArrayList<>();
+        Predicate<Integer> teilbar = i -> i % teiler == 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (teilbar.test(list.get(i))) {
+                teilbareNr.add(list.get(i));
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(main.list.get(i) + " ");
+        }
     }
 
     public void readFile() {
         try {
-            list = Files.lines(new File("numbers.csv").toPath())
-                    .skip(1)
-                    .map(s -> s.split(":"))
-                    .collect(Collectors.toList());
-
+            BufferedReader br = new BufferedReader(new FileReader(new File("numbers.csv")));
+            String line = br.readLine();
+            while (line != null) {
+                list.addAll(Arrays.asList(line.split(":")));
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public
+    public boolean isNumber(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int i = Integer.parseInt(strNum);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+
+    }
+
+    public List<Integer> toIntList() {
+        List<Integer> intList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (isNumber(list.get(i)) == true) {
+                intList.add(Integer.parseInt(list.get(i)));
+            }
+        }
+        return intList;
+    }
 
 }
